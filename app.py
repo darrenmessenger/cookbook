9d0f1f498fd336ec1807b43d933c45c53d2b8bee
Darren Messenger <messengerd888@gmail.com>
2019-03-01 13:30:46 +0000

    Added navigation

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+@app.route('/about')
+def about():
+    return render_template('about.html')
+    
+@app.route('/categories')
+def categories():
+    return render_template('categories.html')
 
 
 if __name__ == '__main__':
