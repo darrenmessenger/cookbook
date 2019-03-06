07bf36cb13d868dc748bcf162297fe52513b2513
Darren Messenger <messengerd888@gmail.com>
2019-03-06 10:49:38 +0000

    Modified .bashrc file so password is secret and connected to database

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+MONGODB_URI = os.getenv("MONGO_URI")
+print("Mongo is connected!")
+print(MONGODB_URI)
+
+app.config["MONGO_DBNAME"] = 'cookbook'
+app.config["MONGO_URI"] = MONGODB_URI
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template("index.html", 
+                           recipes=mongo.db.recipes.find())
+    
 @app.route('/about')
 def about():
     return render_template('about.html')
